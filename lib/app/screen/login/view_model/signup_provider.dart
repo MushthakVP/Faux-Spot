d924ccbf502b3944b473730523eb2f734d5eae73
Mushthak <mail.musthak@gmail.com>
2022-10-11 20:46:22 +0530

    login complete

--- lib/app/screen/login/view_model/signup_provider.dart
+++ lib/app/screen/login/view_model/signup_provider.dart
@@ -1,9 +1,17 @@
+import 'package:faux_spot/app/routes/routes.dart';
+import 'package:faux_spot/app/screen/login/model/emai_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../../../routes/messenger.dart';
+import '../../home/view/home_view.dart';
+import '../service/login_signup_service.dart';
 
 class SignupProvider extends ChangeNotifier {
+  final storage = const FlutterSecureStorage();
   bool loginOrSignup = false;
   bool isLoading = false;
   bool showOtpWidget = false;
+  bool otpSucsess = false;
 
   void changePage() async {
     showOtpWidget = false;
@@ -11,21 +19,69 @@ class SignupProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  bool otpSucsess = false;
-  void sendEmailOtp() async {
+  //================================ VERIFY EMAIL ======================================
+
+  final formKeySignup = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController otpController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void verifyEmail() async {
     isLoading = true;
     notifyListeners();
-    await Future.delayed(const Duration(seconds: 2));
-    showOtpWidget = true;
-    isLoading = false;
-    notifyListeners();
+    if (!formKeySignup.currentState!.validate()) {
+      isLoading = false;
+      notifyListeners();
+      return;
+    }
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    EmailSignupRespones? respones =
+        await LoginSignupService().signupEmail(email, password);
+
+    if (respones != null) {
+      if (respones.error == true) {
+        await storage.write(key: "id", value: respones.id);
+        Messenger.pop(msg: respones.message.toString());
+        showOtpWidget = true;
+        isLoading = false;
+        notifyListeners();
+      } else {
+        isLoading = false;
+        notifyListeners();
+        Messenger.pop(msg: respones.message.toString());
+      }
+    } else {
+      isLoading = false;
+      notifyListeners();
+    }
   }
 
   void verifyOtp() async {
     isLoading = true;
     notifyListeners();
-    await Future.delayed(const Duration(seconds: 2));
-    isLoading = false;
-    notifyListeners();
+
+    if (!formKeySignup.currentState!.validate()) {
+      isLoading = false;
+      notifyListeners();
+      return;
+    }
+    String? id = await storage.read(key: "id");
+    String otp = otpController.text.trim();
+
+    EmailVerifyRespones? respones =
+        await LoginSignupService().verifyEmailOtp(otp, id!);
+    if (respones!.error == true) {
+      storage.write(key: "refreshToken", value: respones.refreshToken);
+      storage.write(key: "token", value: respones.token);
+      Routes.pushreplace(screen: const HomeView());
+      isLoading = false;
+      notifyListeners();
+    } else {
+      Messenger.pop(msg: respones.message.toString());
+      isLoading = false;
+      notifyListeners();
+    }
   }
 }
--- lib/app/service/endpoints.dart
+++ lib/app/service/endpoints.dart
@@ -4,5 +4,7 @@ class EndPoints {
   static const String phoneLogin = "/account/login-number";
   static const String verifyOtp = "/account/verify-number-otp";
   static const String loginEmail = "/account/login-email";
+  static const String signupEmail = "/account/signup-email";
+  static const String emailOtpVerify = "/account/verify-email-otp";
   
 }
